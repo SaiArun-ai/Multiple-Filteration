fa5175a3a7a66cecc4e91cded439ffc75b3e25d4
SaiArun-ai <72151107+SaiArun-ai@users.noreply.github.com>
2021-06-30 11:32:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,11 +5,41 @@ function preload(){
 function setup(){
     canvas = createCanvas(300,300);
     canvas.center();
-    
+    video = createCapture(VIDEO);
+    video.size(440,400);
+    video.hide();
+    //Posenet
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose',gotPoses);
 }
 
-function draw(){}
-
+function draw(){
+    image(video,0,0,440,400);
+}
+var NoseX;
+var NoseY;
+var lEyeX;
+var lEyeY;
+var rEyeX;
+var rEyeY;
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        NoseX = results[0].pose.nose.x;
+        NoseY = results[0].pose.nose.y;
+        lEyeX = results[0].pose.leftEye.x;
+        lEyeY = results[0].pose.leftEye.y;
+        rEyeX = results[0].pose.rightEye.x;
+        rEyeY = results[0].pose.rightEye.y;
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+        console.log("leftEyeX = " + results[0].pose.leftEye.x);
+        console.log("leftEyeY = " + results[0].pose.leftEye.y);
+    }
+}
+function modelLoaded(){
+    console.log("Loaded");
+}
 function TakeSnapshot(){
     save("FilteredImage.png");
 }
