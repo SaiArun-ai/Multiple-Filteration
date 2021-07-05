ba3301cdbcc4cfcd7b0bc8f831e6284cb153cfb3
SaiArun-ai <72151107+SaiArun-ai@users.noreply.github.com>
2021-07-05 18:50:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
+var MORL;
 function preload(){
-
+    Moustache = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
+    Lipstick = loadImage("https://i.postimg.cc/PxFvYgkv/l1.png");
 }
 
 function setup(){
     canvas = createCanvas(300,300);
-    canvas.center();
+    canvas.position(620,325);
     video = createCapture(VIDEO);
     video.size(440,400);
     video.hide();
@@ -15,6 +17,11 @@ function setup(){
 
 function draw(){
     image(video,0,0,440,400);
+    if(MORL == "Lipstick"){
+        image(Lipstick,NoseX ,NoseY+20,40,40);
+    }else if (MORL == "Moustache"){
+        image(Moustache,NoseX - 25,NoseY - 5,40,40);
+    }    
 }
 var NoseX;
 var NoseY;
@@ -22,6 +29,20 @@ var lEyeX;
 var lEyeY;
 var rEyeX;
 var rEyeY;
+function Select(){
+    if(document.getElementById("AorB").value  == "A"){
+        MORL = "Lipstick";        
+        document.getElementById("Filter").style = "display:contents;";
+        document.getElementById("Interact").style = "display:none;";
+    }else if(document.getElementById("AorB").value  == "B"){
+        MORL = "Moustache";
+        document.getElementById("Filter").style = "display:contents;";
+        document.getElementById("Interact").style = "display:none;";
+    }else{
+        document.getElementById("Error").innerHTML = "Type A or B in capital letters(A for lipstick and B for moustache)";
+        document.getElementById("Error").style = "color:red;";
+    }
+}
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
@@ -36,6 +57,11 @@ function gotPoses(results){
         console.log("leftEyeX = " + results[0].pose.leftEye.x);
         console.log("leftEyeY = " + results[0].pose.leftEye.y);
     }
+}
+function Back(){
+    MORL = "";
+    document.getElementById("Filter").style = "display:none;";
+        document.getElementById("Interact").style = "display:contents;";
 }
 function modelLoaded(){
     console.log("Loaded");
